76c86746aea266bd4442ce2de8d396569444b820
gao <1782509343@qq.com>
2018-07-29 12:28:22 +0800

    热销组件和周末推荐

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="content-recommend">
+        <div class="title">热销推荐</div>
+        <ul v-for="item of recommendList" :key="item.id">
+            <li class="item">
+                <div class="item-img">
+                    <img class="img-url" :src='item.imgUrl'/>
+                </div>
+                <div class="item-info">
+                    <p class="item-title" v-text="item.title"></p>
+                    <p class="item-desc" v-text="item.desc"></p>
+                    <button class="item-button">查看详情</button>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+export default{
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '01',
+        title: '南京欢乐水魔方',
+        desc: '五颗星魔幻广场 1543条评论',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/201403/10/03b24acde057acb03c1b514bd2bf638c.jpg_200x200_00cebaa5.jpg'
+      },
+      {
+        id: '02',
+        title: '南京海底世界',
+        desc: '五颗星魔幻广场 1543条评论',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1411/13/0cd5a9a57ae0d1b9b935ec604ce0f9bf.water.jpg_200x200_9b808f51.jpg'
+      }, {
+        id: '03',
+        title: '南京总统府',
+        desc: '五颗星魔幻广场 1543条评论',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1508/f7/f7c3be996152011c.img.jpg_200x200_0b707f70.jpg'
+      }, {
+        id: '04',
+        title: '红山动物园-动物王国',
+        desc: '五颗星魔幻广场 1543条评论',
+        imgUrl: 'http://img1.qunarzz.com/sight/201312/03/38da27f8a2ad319ec8d65eac.jpg_200x200_72a1d26d.jpg'
+      }, {
+        id: '05',
+        title: '南京中国科举博物馆（江南贡院）',
+        desc: '五颗星魔幻广场 1543条评论',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1507/c0/c01ae3666c4d4043.img.jpg_200x200_511a3866.jpg'
+      }
+
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item
+    overflow: hidden
+    display: flex
+    height: 1.9rem
+    .item-img
+      overflow hidden
+      width: 1.7rem
+      height: 1.7rem
+      padding: .1rem
+    .item-info
+      flex: 1
+      padding: .1rem
+      min-width: 0
+      .item-title
+        line-height: .54rem
+        font-size: .32rem
+        ellipsis()
+      .item-desc
+        line-height: .4rem
+        color: #ccc
+        ellipsis()
+      .item-button
+        line-height: .44rem
+        margin-top: .2rem
+        background: #ff9300
+        padding: 0 .2rem
+        border-radius: .06rem
+        color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="content-weekend">
+        <div class="title">周末去哪儿</div>
+        <ul>
+            <li class="item"
+            v-for="item of weekendList"
+            :key="item.id">
+              <div class="item-img-wrapper">
+                    <img class="item-img" :src='item.imgUrl'/>
+              </div>
+                <div class="item-info">
+                    <p class="item-title" v-text="item.title"></p>
+                    <p class="item-desc" v-text="item.desc"></p>
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '01',
+        title: '南京必游TOP10',
+        desc: '看尽王侯将相荣辱成败，六朝金粉潮起潮落',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/f1/ae9d4299b8aaea.jpg_r_640x214_0c555905.jpg'
+      },
+      {
+        id: '02',
+        title: '民国情怀国共风华',
+        desc: '找到你心中想要的那个民国时期的南京',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/e1/cdbf56d837da59.jpg_r_640x214_105fc3f2.jpg'
+      }, {
+        id: '03',
+        title: '孩子的游玩地',
+        desc: '让小朋友们欢脱起来',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/ed/8bce72632c9136.jpg_r_640x214_221c04c6.jpg'
+      },
+      {
+        id: '04',
+        title: '长江烂漫景',
+        desc: '览滚滚长江，看山花烂漫',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1603/ff/19b65f7ca38d70.jpg_r_640x214_c9a4a14b.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    overflow: hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+
+</style>
